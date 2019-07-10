89fb3f41e07cecb27f69c77f9cc8532296fb9d63
sebastosh <sebastosh@gmail.com>
2019-07-09 22:03:59 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,27 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {
+    friends: [],
+}, action){
+
+    switch (action.type) {
+        
+        case 'ADD_FRIEND':
+          return ({
+                friends: [
+                  ...state.friends,
+                  action.friend
+                ]
+            })
+
+  
+        case "REMOVE_FRIEND":
+            const everyOneElseButThem = state.friends.filter(friend => friend.id !== action.id)   
+            return ({
+                friends: everyOneElseButThem
+                
+            })         
+                      
+        default:
+                return state;
+      }
+   
 }
